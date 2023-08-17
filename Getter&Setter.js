dc191e8f67d609106846b3f0bb68dd2b9a832511
Fayiz <fayizcj94.com>
2023-08-17 10:48:29 +0530

    array Methods

--- Getter&Setter.js
+++ Getter&Setter.js
@@ -1,21 +1,33 @@
 class Button{
     constructor(name){
         this.button=document.getElementById('button');
-        this.button=innerHTML =name;
+        this.button.innerHTML=name;
 
         document.body.appendChild(this.button)
     }
 
-    width(width){
-        this.button.style.width=width='px'
+    //setter is using for Assigment operator to Functional operator
+    set width(width){
+        this.button.style.width=width+'px'
     }
 
-    height(height){
-        this.button.style.height=height='px'
+    //setter is using for Assigment operator to Functional operator
+    set height(height){
+        this.button.style.height=height+'px'
     }
+
+    //getter is using for Functional operator to Assigment operator
+    get width(){
+         return this.button.style.width
+    }
+
+    //getter is using for Functional operator to Assigment operator
+    get height(){
+        return this.button.style.height
+   }
 }
 
 var b1=new Button('click me')
-b1.width=100;
-b1.height=100
-console.log(b1);
+b1.width=100;   //calling setter
+b1.height=150;  //calling setter
+console.log(b1.width,b1.height); //calling getter
